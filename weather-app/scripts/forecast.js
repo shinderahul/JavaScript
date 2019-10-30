942fde1a42827fab7f685c63405d7a9ea637ee0e
Rahul Shinde <rahul.shinde@digitaslbi.com>
2019-10-30 18:26:19 +0530

    get weather information

--- weather-app/scripts/forecast.js
+++ weather-app/scripts/forecast.js
@@ -1,7 +1,19 @@
-const key = 'zrtMBuapi1hIWaPqkfTAQwhrdRe5e3nM';
+const key = 'hZf6MypNgir2Ym9uXoyoYg7mTc5apCg6';
+
+
+// get weather information
+const getWeather = async (id) => {
+    console.log(id)
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0]
+}
 
 // get city information
-
 const getCity = async (city) => {
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`
@@ -12,6 +24,8 @@ const getCity = async (city) => {
     return data[0]
 }
 
-getCity('new york')
-    .then(data => { console.log('data', data)})
-    .catch(err => { console.log('err', err)})
+getCity('manchester').then(data => {
+    return getWeather(data.Key)
+  }).then(data => {
+    console.log(data);
+  }).catch(err => console.log(err));
