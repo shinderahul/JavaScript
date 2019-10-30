df612fe18817ed63f30e5239a4770a13edf3a0b4
Rahul Shinde <rahul.shinde@digitaslbi.com>
2019-10-30 17:49:29 +0530

    get city information

--- weather-app/scripts/forecast.js
+++ weather-app/scripts/forecast.js
@@ -1,1 +1,17 @@
 const key = 'zrtMBuapi1hIWaPqkfTAQwhrdRe5e3nM';
+
+// get city information
+
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`
+
+    const response = await fetch(base + query)
+    const data =  await response.json()
+
+    return data[0]
+}
+
+getCity('new york')
+    .then(data => { console.log('data', data)})
+    .catch(err => { console.log('err', err)})
